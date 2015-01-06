c43aed94c0f1fc856de515ebd3b6e79e0b5f9cce
revintec <caoyunbin001@126.com>
2015-01-06 17:04:52 +0800

    not using mingw32-make -j4, casuasing problems building

--- buildall.sh
+++ buildall.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# do not use mingw32-make -j4
+# it seems casuasing problems building ffmpeg
+
 cd x264-snapshot-20141218-2245
 make uninstall
 make clean
 echo cleanup done. configuring...
 ./configure --enable-static
-mingw32-make -j4 install
+make install
 cd ..
 echo ================================
 cd fdk-aac-0.1.3
@@ -13,7 +16,7 @@ make uninstall
 make clean
 echo cleanup done. configuring...
 ./configure
-mingw32-make -j4 install
+make install
 cp /usr/local/bin/libfdk-aac-0.dll ../libfdk-aac-0.dll
 cd ..
 echo ================================
@@ -22,7 +25,7 @@ make uninstall
 make clean
 echo cleanup done. configuring...
 ./configure --enable-gpl --enable-nonfree --extra-cflags="-I/usr/local/include -D_cdecl=__cdecl" --extra-cxxflags="-I/usr/local/include" --extra-ldflags="-L/usr/local/lib" --enable-libfdk-aac --enable-libx264
-mingw32-make -j4
+make
 cp ffmpeg.exe ../ffmpeg.exe
 cd ..
 echo ================================
